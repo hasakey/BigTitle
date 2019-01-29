8f08f0cf930089920cb401a06ffa591a7208e775
dingweiwei <weiwei.ding@justalk.com>
2019-01-29 18:21:47 +0800

    init

---
+++ BigTitle/BigTitle/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  BigTitle
+//
+//  Created by 丁巍巍 on 2019/1/29.
+//  Copyright © 2019年 丁巍巍. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
